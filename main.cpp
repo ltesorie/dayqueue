516244e5045c95eb47176ac8c16a1379f51599b3
Your Name <you@example.com>
2018-04-22 16:50:21 -0400

    hi

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 int main() {
+	queue object;
+	int choice, value;
+	while (1) {
+		cout << "Press 1 to enqueue" << endl;
+		cout << "Press 2 to dequeue" << endl;
+		cout << "Press 3 to display" << endl;
+		cout << "Anything else to quit" << endl;
+		cin >> choice;
+		switch (choice) {
+		case 1: cout << "Add what?" << endl;
+			cin >> value;
+			object.enqueue(value);
+			break;
+		case 2: object.dequeue();
+			break;
+		case 3: object.display();
+			break;
+		default: exit(1);
+		}
 
-	for (int i = 0 ; i < 10 ; i++){
-		return i++;
-}
-//hi 
+		system("pause");
+		return 1;
+	}
 } 
 
