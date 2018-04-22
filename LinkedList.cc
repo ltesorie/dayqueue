5e0ca9d3166d6f3ac6d97ce287737e6cd199b150
Your Name <you@example.com>
2018-04-22 16:52:43 -0400

    newstuff

--- LinkedList.cc
+++ LinkedList.cc
@@ -13,36 +13,12 @@ private:
 		head = NULL;
 		length = 0;
 	}
-
+//looser
 	void insertAtHead(int value) {
 		chunk *body = new chunk;
 		body->value = value;
 		body->next = head;
 		head = body;
-	}
-
-	
-	/*
-	#include <iostream>
-using namespace std;
-
-class chunk {
-public:
-	int value;
-	chunk *next;
-
-	chunk() {
-		value = 0;
-		next = NULL;
-	}
-};
-
-class queue {
-public:
-	chunk *head; 
-
-	queue() {
-		head = NULL;
 	}
 
 	void enqueue(int x) {
@@ -93,35 +69,5 @@ public:
 		}
 		cout << "NULL" << endl;
 	}
-};
-
-int main() {
-	queue object;
-	int choice, value;
-	while (1) {
-		cout << "Press 1 to enqueue" << endl;
-		cout << "Press 2 to dequeue" << endl;
-		cout << "Press 3 to display" << endl;
-		cout << "Anything else to quit" << endl;
-		cin >> choice;
-		switch (choice) {
-		case 1: cout << "Add what?" << endl;
-			cin >> value;
-			object.enqueue(value);
-			break;
-		case 2: object.dequeue();
-			break;
-		case 3: object.display();
-			break;
-		default: exit(1);
-		}
-
-		system("pause");
-		return 1;
-	}
-}
-	*/
-	
-	
 
 };
